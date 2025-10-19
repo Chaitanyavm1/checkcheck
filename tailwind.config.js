9c1c32e6b9bc7fd3c2c16ae65d0fa8ccb695069b
Chaitanyavm1 <vikasmahajan9206@gmail.com>
2025-10-19 13:02:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'chess-light': '#f0d9b5',
+        'chess-dark': '#b58863',
+      }
+    },
+  },
+  plugins: [],
+}
